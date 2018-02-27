06a08b3654711370df6afd71f4b52048c06434b7
Matiquel <Mateusz.pyl@interia.pl>
2018-02-27 09:42:24 +0100

    profil
    elo

--- profil.php
+++ profil.php
@@ -12,72 +12,123 @@
    document.title = "Profil | Transferypilkarskie.pl";
 
  </script>
-  <div class="row">
 
-      <div class="col-md-3">
-        <div class="card" style="height:370px;">
-          <div class="card-body">
-        <div class="user-img">
-          <img src="https://i.imgur.com/2NcD6gE.png" alt="Awatar użytkownika" width="128" height="128">
+   <div class="row">
+
+       <div class="col-md-3">
+         <div class="card" style="height:370px;">
+           <div class="card-body">
+         <div class="user-img">
+           <img src="https://i.imgur.com/2NcD6gE.png" alt="Awatar użytkownika" width="128" height="128">
+         </div>
+         <a href="#" class="badge badge-danger">Admin</a>
+         <a href="#" class="badge badge-success">Moderator</a>
+         <a href="#" class="badge badge-warning">Premium</a>
+         <p class="user-nick cien"><?php echo $user->dane($id_uzytkownika, 'nick');?></p>
+         <div class="list-group" id="list-tab" role="tablist">
+           <a class="list-group-item active" id="list-home-list" data-toggle="tab" href="#list-home" role="tab" aria-controls="home" style="margin-bottom: 5px;">Informacje o użytkowniku</a>
+           <a class="list-group-item active" id="list-profile-list" data-toggle="tab" href="#list-profile" role="tab" aria-controls="profile">Napisz wiadomość</a>
+       </div>
         </div>
-        <a href="#" class="badge badge-danger">Admin</a>
-        <a href="#" class="badge badge-success">Moderator</a>
-        <a href="#" class="badge badge-warning">Premium</a>
-        <p class="user-nick cien"><?php echo $user->dane($id_uzytkownika, 'nick');?></p>
-        <div class="list-group" id="list-tab" role="tablist">
-          <a class="list-group-item active" id="list-home-list" data-toggle="tab" href="#list-home" role="tab" aria-controls="home">Informacje o użytkowniku</a>
-          <a class="list-group-item list-group-item-action disabled" id="list-profile-list" data-toggle="tab" href="#list-profile" role="tab" aria-controls="profile">Napisz wiadomość</a>
       </div>
        </div>
-     </div>
-      </div>
-      <div class="col-md-6">
 
- <div class="cien">
-   <div class="tab-content" id="nav-tabContent">
-     <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">Info</div>
-     <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">Wiadomość</div>
-   </div>
- </div>
-      </div>
-      <div class="col-md-3">
-        <div class="card" style="height:370px;">
-          <div class="card-header">
-              Aktywność użytkownika
-          </div>
+       <div class="col-md-6">
+         <div class="card">
+           <div class="card-header text-center">
+               Informacje o użytkowniku
+           </div>
+         </div>
+             <div class="card-deck">
+               <div class="card" style="margin-top: 18px">
+               <div class="card-body">
+                 <div class="row">
+                  <div class="col-md-12 text-center">
+                    <span class="text-center">Ulubiona liga: Ekstraklasa </span>
+                  </div>
+                </div>
+              </div>
+             </div>
+           </div>
+
+            <div class="card-deck">
+             <div class="card" style="margin-top: 18px">
+             <div class="card-body">
+               <div class="row">
+                <div class="col-md-12 text-center">
+                  <span class="text-center">Ulubiony klub: Lechia Gdańsk</span>
+                </div>
+              </div>
+            </div>
+           </div>
+
+            </div>
+
+            <div class="card-deck">
+             <div class="card" style="margin-top: 18px">
+             <div class="card-body">
+               <div class="row">
+                <div class="col-md-12 text-center">
+                  <span class="text-center">Ulubiony zawodnik: Sławek Peszko</span>
+                </div>
+              </div>
+            </div>
+           </div>
+         </div>
+
+         <div class="card-deck">
+          <div class="card" style="margin-top: 18px">
           <div class="card-body">
-
             <div class="row">
-              <div class="col-md-12" style="text-align:center;margin-top:5px;border-bottom:2px solid #2196f3;">
-                <span>Oddanych Głosów:</span>
-                <h4 class="counter">500</h4>
-              </div>
-            </div>
-
-            <div class="row">
-              <div class="col-md-12" style="text-align:center;margin-top:5px;border-bottom:2px solid #2196f3;">
-                <span>Komentarzy:</span>
-                <h4 class="counter">500</h4>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-12" style="text-align:center;margin-top:5px;border-bottom:2px solid #2196f3;">
-                <span>Polubień:</span>
-                <h4 class="counter">13300</h4>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-12" style="text-align:center;margin-top:5px;border-bottom:2px solid #2196f3;">
-                <span>Rekomendacji:</span>
-                <h4 class="counter">5</h4>
-              </div>
-            </div>
-
-       </div>
-     </div>
+             <div class="col-md-12 text-center">
+               <span class="text-center">Ranga: Debil</span>
+             </div>
+           </div>
+         </div>
+        </div>
       </div>
+         </div>
 
-  </div>
+
+
+       <div class="col-md-3">
+         <div class="card" style="height:370px;">
+           <div class="card-header text-center">
+               Aktywność użytkownika
+           </div>
+           <div class="card-body">
+
+             <div class="row">
+               <div class="col-md-12" style="text-align:center;margin-top:5px;border-bottom:2px solid #2196f3;">
+                 <span>Oddane głosy:</span>
+                 <h4 class="counter">500</h4>
+               </div>
+             </div>
+
+             <div class="row">
+               <div class="col-md-12" style="text-align:center;margin-top:5px;border-bottom:2px solid #2196f3;">
+                 <span>Komentarze:</span>
+                 <h4 class="counter">500</h4>
+               </div>
+             </div>
+             <div class="row">
+               <div class="col-md-12" style="text-align:center;margin-top:5px;border-bottom:2px solid #2196f3;">
+                 <span>Polubienia:</span>
+                 <h4 class="counter">13300</h4>
+               </div>
+             </div>
+             <div class="row">
+               <div class="col-md-12" style="text-align:center;margin-top:5px;border-bottom:2px solid #2196f3;">
+                 <span>Rekomendacji:</span>
+                 <h4 class="counter">5</h4>
+               </div>
+             </div>
+
+        </div>
+      </div>
+       </div>
+
+   </div>
 
 
 
