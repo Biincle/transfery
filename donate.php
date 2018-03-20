b173145a16beaea0624ef626cb778ccdd2056334
Biincle <pawelek599@gmail.com>
2018-03-20 10:06:08 +0100

    Added donate buttons

---
+++ donate.php
@@ -0,0 +1,43 @@
+<?php
+include("header.php");
+?>
+<script type="text/javascript">
+    document.title = 'Donate - Transferypilkarskie.pl';
+</script>
+
+<div class="row">
+
+<div class="col-md-4">
+<div class="card">
+  <div class="card-body" style="text-align:center;">
+    <img src="img/PayPal.png" class="img-responsive" width="250" height="66">
+    <hr>
+     <a href="#" class="btn btn-success btn-raised">Wpłać</a>
+  </div>
+</div>
+</div>
+
+<div class="col-md-4">
+<div class="card">
+  <div class="card-body" style="text-align:center;">
+    <img src="img/homepay.png" class="img-responsive" width="250" height="66">
+    <hr>
+     <a href="#" class="btn btn-success btn-raised">Wpłać</a>
+  </div>
+</div>
+</div>
+
+<div class="col-md-4">
+<div class="card">
+  <div class="card-body" style="text-align:center;">
+    <img src="img/btc.png" class="img-responsive" width="250" height="">
+    <hr>
+     <a href="#" class="btn btn-success btn-raised">Wpłać</a>
+  </div>
+</div>
+</div>
+
+</div>
+
+
+<?php include("footer.php");?>
